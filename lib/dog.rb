9a60d657fcad4d3bfbd48893087bd973aea780d3
Amanda Ackerman <ackerm44@gmail.com>
2017-11-28 19:51:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
       WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, id)
+    dog = DB[:conn].execute(sql, id)
 
     dog_data = dog[0]
     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
