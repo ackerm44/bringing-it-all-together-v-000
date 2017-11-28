0a2d8040417409cdea68d54cca19214180aa93d9
Amanda Ackerman <ackerm44@gmail.com>
2017-11-28 20:01:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,7 +70,7 @@ class Dog
     dog = DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
-
+    binding.pry
     dog_data = dog[0]
     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end
