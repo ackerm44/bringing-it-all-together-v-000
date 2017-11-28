9e629879aebc4a1df40dc3b31a080141a69bd2e9
Amanda Ackerman <ackerm44@gmail.com>
2017-11-28 19:55:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,19 @@ class Dog
     dog.save
     dog
   end
+  
+  def self.new_from_db(row)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def find_by_name(name)
+    sql <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
 
   def self.find_by_id(id)
     sql = <<-SQL
@@ -68,18 +81,7 @@ class Dog
     end
   end
 
-  def self.new_from_db(row)
-    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
-  end
 
-  def find_by_name(name)
-    sql <<-SQL
-      SELECT * FROM dogs
-      WHERE name = ?
-    SQL
-
-    DB[:conn].execute(sql, name)
-  end
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
